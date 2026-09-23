8bed12c644222a66af73b1f40dd60cf347503ad8
nryanA00086006 <A00086006@myTUDublin.ie>
2026-09-23 19:55:51 +0100

    add mstest

---
+++ bmi2026labtest/Test1.cs
@@ -0,0 +1,30 @@
+﻿namespace bmi2026labtest;
+
+using BMICalculator;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    [DataRow(9, 0, 6, 0, BMICategory.Underweight)]
+    [DataRow(11, 0, 6, 0, BMICategory.Normal)]
+    [DataRow(14, 0, 6, 0, BMICategory.Overweight)]
+    [DataRow(20, 0, 6, 0, BMICategory.Obese)]
+    public void BMICategory_ReturnsExpectedCategory(
+        int weightStones,
+        int weightPounds,
+        int heightFeet,
+        int heightInches,
+        BMICategory expectedCategory)
+    {
+        var bmi = new BMI
+        {
+            WeightStones = weightStones,
+            WeightPounds = weightPounds,
+            HeightFeet = heightFeet,
+            HeightInches = heightInches
+        };
+
+        Assert.AreEqual(expectedCategory, bmi.BMICategory);
+    }
+}
